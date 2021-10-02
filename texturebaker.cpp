a5f72172f84e501e1cb176f2574024865ab397c6
debapritam <debapritam.chakra@daimler.com>
2021-10-03 04:01:36 +0530

    change3

---
+++ texturebaker.cpp
@@ -0,0 +1,35 @@
+#include "texturebaker.h"
+
+#include "glcache.h"
+
+TextureBaker::TextureBaker(QWidget *parent) : QOpenGLWidget(parent)
+{
+
+}
+
+// returns true on successful save
+bool TextureBaker::writeTextureToFile(Mesh *mesh)
+{
+    makeCurrent();
+
+    if (!GLCache::hasMeshTexture(mesh)) {
+        return false;
+    }
+
+    GLuint textureId = GLCache::meshTextureId(mesh);
+    QString path = mesh->texturePath();
+    const int size = mesh->textureSize();
+
+    const int NUM_COLOR_CHANNELS = 3;
+    GLubyte* data = new GLubyte[size* size * NUM_COLOR_CHANNELS];
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textureId);
+    glGetTexImage(GL_TEXTURE_2D, 0, GL_RGB, GL_UNSIGNED_BYTE, (void*)data);
+
+    QImage out(data, size, size, QImage::Format_RGB888);
+    out.mirrored().save(path);
+
+    return true;
+}
+
